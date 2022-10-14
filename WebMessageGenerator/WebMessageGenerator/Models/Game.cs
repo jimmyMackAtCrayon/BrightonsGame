439a9a304778d542ae16379952b551e347e8efc1
McAliece <jim.mcaliece@crayon.com>
2022-10-14 05:46:05 -0400

    Initial Release, only send message to EventHub

---
+++ WebMessageGenerator/WebMessageGenerator/Models/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace WebMessageGenerator.Models
+{
+    public class Game
+    {
+        public string GameId { get; set; }
+        public string Name { get; set; }
+        public string GameDate { get; set; }
+    }
+}
---
+++ WebMessageGenerator/WebMessageGenerator/Models/Send.cs
@@ -0,0 +1,9 @@
+﻿namespace WebMessageGenerator.Models
+{
+    public class Send
+    {
+        public string GameId { get; set; }
+        public string Name { get; set; }
+        public string GameDate { get; set; }
+    }
+}
